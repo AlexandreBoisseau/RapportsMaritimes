2099e0c31f7f7e2fae9f14519cb3efdec597b55e
Alexandre Boisseau <alexandre.boisseau@epita.fr>
2023-04-15 09:15:10 +0200

    Image resizer modal

---
+++ main.py
@@ -0,0 +1,25 @@
+# main_app.py
+
+import tkinter as tk
+from image_resizer_modal import ImageResizerModal
+
+class YourMainApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Your Main App")
+        self.geometry("800x600")
+
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.show_image_resizer_button = tk.Button(self, text="Ouvrir le redimensionneur d'image", command=self.show_modal)
+        self.show_image_resizer_button.pack(pady=10)
+
+    def show_modal(self):
+        image_resizer_modal = ImageResizerModal(self)
+        image_resizer_modal.grab_set()
+
+if __name__ == "__main__":
+    app = YourMainApp()
+    app.mainloop()
