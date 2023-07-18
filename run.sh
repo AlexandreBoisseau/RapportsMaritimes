f11fabeabbb661e45e919288141767e5088fcb42
Alexandre Boisseau <alexandre.boisseau@epita.fr>
2023-07-18 14:40:12 +0200

    add run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+python3.11 rapport.py
+
+rm rapport.tex 2>/dev/null
+rm rapport.aux 2>/dev/null
+rm rapport.log 2>/dev/null
+rm rapport.out 2>/dev/null
+rm rapport.toc 2>/dev/null
+rm rapport.tex.bak 2>/dev/null
+rm transparant.webp 2>/dev/null
+rm rapport.pdf 2>/dev/null
